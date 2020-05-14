8a535cd657bcbb5f91bf7ed24538fc17976558ad
Nicole Wang <nicole.wang@yale.edu>
2020-05-14 20:02:17 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
 min_value = name_hash[0][0]
 name_hash.each do |key, value|
-  if value< min_value
+  if value < min_value
     min_value = value
   end 
   min_value
