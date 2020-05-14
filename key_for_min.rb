44bcacfaf51d3c5c8477bf3b83f438193f8cd292
Nicole Wang <nicole.wang@yale.edu>
2020-05-14 20:07:33 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,14 +2,6 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-min_value = name_hash[0][0]
-
-name_hash.each do |key, value|
-  if value < min_value
-    min_value = value
-    
-  end 
-end 
-  min_value
+name_hash.each {|key, value| puts k if v == hash.values.max }
 
 end
