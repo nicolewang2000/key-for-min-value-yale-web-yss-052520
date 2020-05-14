716a73b3b9e0382711f1e236603f7e21a20f7d81
Nicole Wang <nicole.wang@yale.edu>
2020-05-14 20:09:22 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+min_value = 
+
 name_hash.each do |key, value|
-  
+  if value > max_value
+    max_value = value
+    key_for_max_value = key
+  end
+end
 
 end
